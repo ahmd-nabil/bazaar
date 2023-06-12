ecd48c795cd0c52a3091802e9132513f1189124f
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-12 23:23:11 +0300

    initial commit

---
+++ src/main/java/nabil/bazaar/BazaarApplication.java
@@ -0,0 +1,13 @@
+package nabil.bazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazaarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nabil/bazaar/BazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package nabil.bazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
